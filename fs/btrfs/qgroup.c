3d01471345df29ae5934d3d15ea95dfa44602c8b
Qu Wenruo <wqu@suse.com>
2024-02-27 11:03:52 +0800

    btrfs: qgroup: verify btrfs_qgroup_inherit parameter
    [BUG]
    Currently btrfs can create subvolume with an invalid qgroup inherit
    without triggering any error:
    
     # mkfs.btrfs -O quota -f $dev
     # mount $dev $mnt
     # btrfs subvolume create -i 2/0 $mnt/subv1
     # btrfs qgroup show -prce --sync $mnt
     Qgroupid    Referenced    Exclusive   Path
     --------    ----------    ---------   ----
     0/5           16.00KiB     16.00KiB   <toplevel>
     0/256         16.00KiB     16.00KiB   subv1
    
    [CAUSE]
    We only do a very basic size check for btrfs_qgroup_inherit structure,
    but never really verify if the values are correct.
    
    Thus in btrfs_qgroup_inherit() function, we have to skip non-existing
    qgroups, and never return any error.
    
    [FIX]
    Fix the behavior and introduce extra checks:
    
    - Introduce early check for btrfs_qgroup_inherit structure
      Not only the size, but also all the qgroup ids would be verifyed.
    
      And the timing is very early, so we can return error early.
      This early check is very important for snapshot creation, as snapshot
      is delayed to transaction commit.
    
    - Drop support for btrfs_qgroup_inherit::num_ref_copies and
      num_excl_copies
      Those two members are used to specify to copy refr/excl numbers from
      other qgroups.
      This would definitely mark qgroup inconsistent, and btrfs-progs has
      dropped the support for them for a long time.
      It's time to drop the support for kernel.
    
    - Verify the supported btrfs_qgroup_inherit::flags
      Just in case we want to add extra flags for btrfs_qgroup_inherit.
    
    Now above subvolume creation would fail with -ENOENT other than silently
    ignore the non-existing qgroup.
    
    CC: stable@vger.kernel.org
    Signed-off-by: Qu Wenruo <wqu@suse.com>

--- fs/btrfs/qgroup.c
+++ fs/btrfs/qgroup.c
@@ -3046,6 +3046,58 @@ int btrfs_run_qgroups(struct btrfs_trans_handle *trans)
 	return ret;
 }
 
+int btrfs_qgroup_check_inherit(struct btrfs_fs_info *fs_info,
+			       struct btrfs_qgroup_inherit *inherit,
+			       size_t size)
+{
+	if (inherit->flags & ~BTRFS_QGROUP_INHERIT_FLAGS_SUPP)
+		return -EOPNOTSUPP;
+	if (size < sizeof(*inherit) || size > PAGE_SIZE)
+		return -EINVAL;
+
+	/*
+	 * In the past we allow btrfs_qgroup_inherit to specify to copy
+	 * refr/excl numbers directly from other qgroups.
+	 * This behavior has been disable in btrfs-progs for a very long time,
+	 * but here we should also disable them for kernel, as this behavior
+	 * is known to mark qgroup inconsistent, and a rescan would wipe out the
+	 * change anyway.
+	 *
+	 * So here we just reject any btrfs_qgroup_inherit with num_ref_copies or
+	 * num_excl_copies.
+	 */
+	if (inherit->num_ref_copies || inherit->num_excl_copies)
+		return -EINVAL;
+
+	if (inherit->num_qgroups > PAGE_SIZE)
+		return -EINVAL;
+
+	if (size != struct_size(inherit, qgroups, inherit->num_qgroups))
+		return -EINVAL;
+
+	/*
+	 * Now check all the remaining qgroups, they should all:
+	 * - Exist
+	 * - Be higher level qgroups.
+	 */
+	for (int i = 0; i < inherit->num_qgroups; i++) {
+		struct btrfs_qgroup *qgroup;
+		u64 qgroupid = inherit->qgroups[i];
+
+		if (btrfs_qgroup_level(qgroupid) == 0)
+			return -EINVAL;
+
+		spin_lock(&fs_info->qgroup_lock);
+		qgroup = find_qgroup_rb(fs_info, qgroupid);
+		if (!qgroup) {
+			spin_unlock(&fs_info->qgroup_lock);
+			return -ENOENT;
+		}
+		spin_unlock(&fs_info->qgroup_lock);
+	}
+	return 0;
+}
+
 static int qgroup_auto_inherit(struct btrfs_fs_info *fs_info,
 			       u64 inode_rootid,
 			       struct btrfs_qgroup_inherit **inherit)
