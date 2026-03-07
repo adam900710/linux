553c72605efc15a18548bf60aae57e9398d19e13
Qu Wenruo <wqu@suse.com>
2026-03-08 09:22:29 +1030

    btrfs: enable experimental delayed compression support
    Instead of the existing async submission path, the new delayed bbio will
    handle compressed writes by:
    
    - Allocating delayed em/oe at run_delalloc_*() time
      Thus no data extent is reserved at that time.
    
    - Delayed bbio will be assembled at extent_writepage_io() time
    
    - Delayed bbio will be intercepted just before submission
      Which will run compression (or fallback to uncompressed writes) in
      workqueue.
      Data extents will only be reserved at that time, and the delayed em
      will be replaced by real ones.
    
      Meanwhile the real OE will be added as a child of the parent delayed
      OE, and when the parent OE finishes, the child OE will be finished
      with their file extents inserted.
    
    This has some benefits:
    
    - Higher concurrency
      Previously async submission will hold the folio and io tree range
      locked, this means we cannot even read the uptodate folio.
    
      Furthermore although the compressed write is queued into a workqueue
      for submission and extent_writepage_io() will skip the compressed
      range, when we need to write the next folio of the compressed range,
      we will need to wait for the folio to be unlocked.
    
      This makes async submission less async.
    
    - Future DONTCACHE writes support
      We do not support DONTCACHE because that feature requires writeback path
      to clear the folio dirty and submit them sequentially.
    
      Meanwhile async submission makes the writeback async, breaking the
      sequential submission requirement.
    
      This is also why we need complex per-block tracking for writeback
      flags, while iomap only requires a counter tracking.
    
      With the new delayed compression, the lifespan of a folio aligns with
      DONTCACHE and iomap.
    
    However there is also a change to the fsync() behavior:
    
    - Now an inode with compressed write will require full sync
      We can not do any fast sync, as that will grab the info from OE
      directly, but since delayed OE is just a place holder, all the OE info
      can not be trusted until the parent OE is finished.
    
    There is an extra handling for defrag, where we have to write and wait
    for the defrag range.
    This is to avoid clearing inode->defrag_compress before the compression
    starts.
    
    Finally this new experimental feature is not yet compatible with zoned,
    so for zoned btrfs it will go through the old compression path.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -1659,6 +1659,68 @@ static bool run_delalloc_compressed(struct btrfs_inode *inode,
 	return true;
 }
 
+static int run_delalloc_delayed(struct btrfs_inode *inode, struct folio *locked_folio,
+				u64 start, u64 end)
+{
+	struct btrfs_root *root = inode->root;
+	struct btrfs_fs_info *fs_info = root->fs_info;
+	struct extent_state *cached = NULL;
+	u64 cur = start;
+	int ret;
+
+	if (btrfs_is_shutdown(fs_info)) {
+		ret = -EIO;
+		goto error;
+	}
+	while (cur < end) {
+		struct extent_map *em;
+		struct btrfs_ordered_extent *oe;
+		u32 cur_len = min_t(u64, end + 1 - cur, BTRFS_MAX_COMPRESSED);
+
+		btrfs_lock_extent(&inode->io_tree, cur, cur + cur_len - 1, &cached);
+		em = btrfs_create_delayed_em(inode, cur, cur_len);
+		if (IS_ERR(em)) {
+			ret = PTR_ERR(em);
+			goto error;
+		}
+		btrfs_free_extent_map(em);
+		oe = btrfs_alloc_delayed_ordered_extent(inode, cur, cur_len);
+		if (IS_ERR(oe)) {
+			btrfs_drop_extent_map_range(inode, cur, cur + cur_len - 1, false);
+			ret = PTR_ERR(oe);
+			goto error;
+		}
+		btrfs_put_ordered_extent(oe);
+
+		cur += cur_len;
+	}
+	/*
+	 * Force the next fsync to wait for any delayed OE to finish,
+	 * as the fast path cannot handle delayed OE correctly.
+	 */
+	btrfs_set_inode_full_sync(inode);
+	extent_clear_unlock_delalloc(inode, start, end, locked_folio, &cached,
+				     EXTENT_LOCKED | EXTENT_DELALLOC,
+				     PAGE_UNLOCK);
+	return 0;
+error:
+	if (start < cur) {
+		btrfs_drop_extent_map_range(inode, start, cur - 1, false);
+		btrfs_cleanup_ordered_extents(inode, start, cur - start);
+		extent_clear_unlock_delalloc(inode, start, cur - 1, locked_folio, &cached,
+			EXTENT_LOCKED | EXTENT_DELALLOC,
+			PAGE_UNLOCK | PAGE_START_WRITEBACK | PAGE_END_WRITEBACK);
+	}
+	if (cur < end) {
+		extent_clear_unlock_delalloc(inode, cur, end, locked_folio, &cached,
+			EXTENT_LOCKED | EXTENT_DELALLOC | EXTENT_DELALLOC_NEW |
+			EXTENT_DEFRAG | EXTENT_DO_ACCOUNTING,
+			PAGE_UNLOCK | PAGE_START_WRITEBACK | PAGE_END_WRITEBACK);
+		btrfs_qgroup_free_data(inode, NULL, cur, end + 1 - cur, NULL);
+	}
+	return ret;
+}
+
 /*
  * Run the delalloc range from start to end, and write back any dirty pages
  * covered by the range.
@@ -2439,9 +2501,12 @@ int btrfs_run_delalloc_range(struct btrfs_inode *inode, struct folio *locked_fol
 		return run_delalloc_nocow(inode, locked_folio, start, end);
 
 	if (btrfs_inode_can_compress(inode) &&
-	    inode_need_compress(inode, start, end, false) &&
-	    run_delalloc_compressed(inode, locked_folio, start, end, wbc))
-		return 1;
+	    inode_need_compress(inode, start, end, false)) {
+		if (IS_ENABLED(CONFIG_BTRFS_EXPERIMENTAL) && !zoned)
+			return run_delalloc_delayed(inode, locked_folio, start, end);
+		else if (run_delalloc_compressed(inode, locked_folio, start, end, wbc))
+			return 1;
+	}
 
 	if (zoned)
 		return run_delalloc_cow(inode, locked_folio, start, end, wbc, true);
